63d85c4c60ccd07f1a6fb1eae67915804af74cd4
karolynnnnnn <129324547+karolynnnnnn@users.noreply.github.com>
2024-08-12 15:47:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+const perguntas = [
+    {
+        enunciado: "Gabrielly estava andando pelo seu bairro quando encontra a sua paixão de infancia o João, eles se davam muito bem mas depois que ela disse que gostava dele, eles se afastaram.Ele a ve de longe e sorri, o que ela faz?"
+        alternativas: [
+            "vai falar com ele",
+            "ignora e sai andando."
+        ]
+    },
+    {
+        enunciado: "Depois de ignorar João, Gabrielly decide ir para sua casa. Chegando lá, ela vai direto para seu quarto e seu celular toca, era Léo seu namorado.O que ela faz?",
+        alternativas: [
+            "Atende o celular e fala para ele que precisa conversar com ele, pois ela estava confusa e não sabia se gostaria de continuar seu relacionamento.",
+            "Ignora o telefonema e vai tomar um banho para air com as suas amigas.",
+        ]
+    },
+    {
+        enunciado: "Depois da conversa com ele, vocês decidiram sair para resolver essa situação, ele:",
+        alternativas: [
+            "Não aparece no lugar marcado e termina com você pelo celular",
+            "Aparece no local marcado e vocês se resolvem"
+        ]
+    },
+    {
+        enunciado: "João te vê sentada sozinha e vai em sua direção, ele te fala que pensou sobre o que você disse alguns dias atrás e fala que te ama e não consegue te esquecer, você:",
+        alternativas: [
+            "da um beijo nele ediz que não consegue viver sem ele",
+            "fala que já reprimiu seus sntimentos e agora voceê quem não gosta dele"
+        ]
+    },
+    {
+        enunciado: "Gabrielly e João...Acabam ficando juntos, eles se casam depois de 4 anos juntos e vivem felizes até hoje, diz lenda que eles viveram juntos e tiveram dois filhos que se tornaram astros do cinema, Caio Castro e Bruna Marquezine.",
+         
+          
+        
+    },
+];
